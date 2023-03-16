ff2efefeb16a9d3746c073b19561a54dad1d2994
Rosanna Birtwistle <rosa.birter@gmail.com>
2023-03-16 17:42:40 +0000

    16/03/2023

--- Virtual Pet game/Virtual Pet game.cpp
+++ Virtual Pet game/Virtual Pet game.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string.h>
+#include <vector>
 using namespace std;
 
 //Declare classes: customers (young ,pensioner, premium?), pets, other?
@@ -8,16 +9,24 @@ class Customer
 {
 protected:
 	string customerName;
-	string address;
 	int age;
 public:
-	Customer(string name);
-	string getCustomerName() { return customerName; }
+	Customer(string nameIn, int ageIn);
+	string getName() { return customerName; }
+	int getAge() { return age; }
+	void display();
 };
 
-Customer::Customer(string name)
+Customer::Customer(string nameIn, int ageIn)
 {
-	customerName = name;
+	customerName = nameIn;
+	age = ageIn;
+}
+
+void Customer::display()
+{
+	cout << "Name: " << getName() << endl;
+	cout << "Age: " << getAge() << endl;
 }
 
 class Pet
@@ -32,12 +41,58 @@ protected:
 	int popularity;
 	char type;
 public:
+	Pet(string nameIn, char typeIn);
 };
+
+Pet::Pet(string nameIn, char typeIn)
+{
+	petName = nameIn;
+	type = typeIn;
+}
+
+vector<Customer> customerList;
+
+void createCustomer();
+
+void displayMenu()
+{
+	cout << "Welcome, user! What would you like to do?" << endl << endl;
+	cout << "Make a new customer (M)" << endl;
+	cout << "Display customer list (D)" << endl;
+	cout << "View pet details (V)" << endl;
+
+	string input;
+
+	cin >> input;
+
+	if (input == "M" || input == "m") createCustomer();
+}
+
+void createCustomer()
+{
+	cout << "Please enter the customer's first name" << endl;
+	string nameIn;
+	int ageIn;
+
+	cin >> nameIn;
+
+	cout << "Please enter the customer's age" << endl;
+
+	cin >> ageIn;
+
+	cout << endl;
+
+	Customer newCustomer(nameIn, ageIn);
+	customerList.push_back(newCustomer);
+
+	newCustomer.display();
+
+
+
+
+}
 
 int main()
 {
-
-	Customer newCustomer("Ro");
-
-	cout << "customer's name is " << newCustomer.getCustomerName() << endl;
+	displayMenu();
 }
